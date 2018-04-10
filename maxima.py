926e7fa4f65eedcf90119da84593f980c080d979
Tiziano Zito <opossumnano@gmail.com>
2018-04-10 11:15:08 +0200

    my solution for maxima

--- maxima.py
+++ maxima.py
@@ -2,23 +2,41 @@ def find_maxima(x):
     """Find local maxima of x.
 
     Example:
-    >>> x = [1, 2, 3, 2, 4, 3]
+    >>> x = [1, 2, 3, 1, 4, 3]
     >>> find_maxima(x)
     [2, 4]
 
+    If in a local maximum several elements have the same value,
+    return the rightmost index.
+    Example:
+    >>> x = [1, 2, 2, 2, 1]
+    >>> find_maxima(x)
+    [3]
+
     Input arguments:
-        x -- 1D list numbers
+        x -- 1D list of numbers
 
     Output:
         idx -- list of indices of the local maxima in x
     """
 
     idx = []
+    up = False
+    down = False
     for i in range(len(x)):
-        if i == len(x) - 1:
-            if x[i-1] < x[i]:
-                idx.append(i)
-        # `i` is a local maximum if the signal decreases before and after it
-        elif x[i-1] < x[i] and x[i+1] < x[i]:
+        if i == 0 or x[i-1] < x[i]:
+            up = True
+        elif x[i-1] > x[i]:
+            up = False
+
+        # if x[i-1] == x[i], no change
+
+        if i+1 == len(x) or x[i+1] < x[i]:
+            down = True
+        elif x[i+1] > x[i]:
+            down = False
+
+        if up and down:
             idx.append(i)
+
     return idx
